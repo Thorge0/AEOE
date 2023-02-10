cd632d194643f12f5aa8c3ef1400bfc2a3e79863
Akutasan <mirodurana1@gmail.com>
2023-02-10 09:46:46 +0100

    Ziel 1 Push

--- src/AEOE.java
+++ src/AEOE.java
@@ -1,8 +1,19 @@
 import java.util.Arrays;
 
 public class AEOE {
-    public static void main(String[] args){
-        char[][] ZDArr = zweiDimReihungen.zufallsZeichenreihung(4,5);
+    public static void main(String[] args) {
+        ziel1_aufgabe1();
+    }
+
+    /**
+     *
+     *
+     */
+    public static void ziel1_aufgabe1(){
+        char[][] ZDArr = zweiDimReihungen.zufallsZeichenreihung(5, 20);
+        System.out.println(Arrays.deepToString(ZDArr).length() - Arrays.deepToString(ZDArr).replaceAll(String.valueOf(IO.getChar("Give me Char.")), "").length());
+        System.out.println(Arrays.deepToString(ZDArr));
+
         char gesucht = IO.getChar("Give me Char.");
         int counter = 0;
 
