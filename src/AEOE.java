23e262e389a3cbe5fa8dc3eda7f80e3cb20851d9
Akutasan <mirodurana1@gmail.com>
2023-02-10 10:37:38 +0100

    Ziel 1, methoden kommentare

--- src/AEOE.java
+++ src/AEOE.java
@@ -7,6 +7,11 @@ public class AEOE {
         ziel1_aufgabe3();
     }
 
+    /**
+     * Methode ziel1_aufgabe3
+     *
+     * Von Reihung aus Ganzzahlen die größte und die kleinste Zahl ermittelt
+     */
     public static void ziel1_aufgabe3() {
         int[][] ZDArr = zweiDimReihungen.zufallsZahlenreihung(5, 20);
         int value = ZDArr[0][0];
@@ -31,6 +36,12 @@ public class AEOE {
         System.out.println(Arrays.deepToString(ZDArr));
     }
 
+    /**
+     * Methode ziel1_aufgabe2
+     *
+     * Methode die das kleine 1x1 bis zur Zahl übergibt.
+     * @param zahl wie oft die Multiplikation stattfinden soll
+     */
     public static void ziel1_aufgabe2(int zahl) {
         String[][] daskleine1x1 = new String[zahl][10];
         for (int i = 0; i < zahl; i++) {
@@ -41,6 +52,11 @@ public class AEOE {
         System.out.println(Arrays.deepToString(daskleine1x1));
     }
 
+    /**
+     * Methode ziel1_aufgabe1
+     *
+     * Eine 2D Reihung und ein Zeichen übergeben bekommt und als Ganzzahl zurückgibt, wie oft dieses Zeichen enthalten ist.
+     */
     public static void ziel1_aufgabe1() {
         char[][] ZDArr = zweiDimReihungen.zufallsZeichenreihung(5, 20);
         // Kurze Variante
